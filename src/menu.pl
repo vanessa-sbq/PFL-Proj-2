c944923b473b05a847cb103593bda83e81ea6ae7
vanessa-sbq <up202207919@up.pt>
2025-01-03 18:06:33 +0000

    Implemented a predicate to read user input correctly

--- src/menu.pl
+++ src/menu.pl
@@ -1,5 +1,22 @@
 cls :- write('\33\[2J').
 
+read_string(Prompt, InputAtom) :-
+    write(Prompt),
+    flush_output, 
+    read_line(InputCodes),
+    atom_codes(InputAtom, InputCodes), % Convert character codes to an atom
+    format('You entered: ~w~n', [InputAtom]).
+
+read_integer(Prompt, Number) :-
+    write(Prompt),
+    flush_output,
+    read_line(InputCodes), % Read input as a list of character codes
+    (catch(number_codes(Number, InputCodes), _, fail) -> % Convert to integer
+     true;   
+     write('Invalid input, please try again.'), nl,
+     read_integer(Prompt, Number)  % Retry on invalid input
+    ), !.
+
 displayMenu :- write('                                                 '), nl,
                write('8b    d8    db    88""Yb 88   88 88        db    '), nl,
                write('88b  d88   dPYb   88__dP 88   88 88       dPYb   '), nl,
@@ -8,13 +25,13 @@ displayMenu :- write('                                                 '), nl,
                write('                                                 '), nl.
 
 
-displayOptions(X) :- repeat,
-                     write('1 - Human vs. Human'), nl,
+displayOptions(X) :- write('1 - Human vs. Human'), nl,
                      write('2 - Human vs. Computer'), nl,
                      write('3 - Computer vs. Computer'), nl, nl,
-                     write('Game mode: '),
-                     read(X),
-                     integer(X), 
-                     X >= 1, 
-                     X =< 3,
-                     !.
+                     repeat,
+                     read_integer('Game mode: ', X),
+                     (X >= 1, X =< 3 -> !;   
+                      write('Invalid option, please try again.'), nl, fail).
+
+
+
